9c3e51487efe314a0b1559bb81e9d0102798e7bd
Christian Sayson Romano <romanozchristian@gmail.com>
2026-03-30 23:40:44 +0800

    Add projectsData array to data.js

---
+++ data.js
@@ -0,0 +1,44 @@
+export const projectsData = [
+    {
+        img: "/assets/images/joshuagarcia.jpg",
+        category: "video",
+        title: "Webiste",
+        description: "Idk",
+        gradient: "from-blue-900/50 to-black",
+    },
+    {
+        img: "/assets/images/byroun.png",
+        category: "web",
+        title: "Project",
+        description: "Lorem ipsum mwa mwa",
+        gradient: "from-gold/20 to-black",
+    },
+    {
+        img: "/assets/images/byroun.png",
+        category: "brand",
+        title: "Project",
+        description: "Lorem ipsum mwa mwa",
+        gradient: "from-purple-900/40 to-black",
+    },
+    {
+        img: "/assets/images/byroun.png",
+        category: "video",
+        title: "Project",
+        description: "Lorem ipsum mwa mwa",
+        gradient: "from-sky-900/40 to-black",
+    },
+    {
+        img: "/assets/images/byroun.png",
+        category: "web",
+        title: "Project",
+        description: "Lorem ipsum mwa mwa",
+        gradient: "from-emerald-900/30 to-black",
+    },
+    {
+        img: "/assets/images/byroun.png",
+        category: "brand",
+        title: "idk",
+        description: "Lorem ipsum mwa mwa",
+        gradient: "from-rose-900/30 to-black",
+    },
+];
