b69b01b672e9d0b8dafee86ec0e98f96fb26876e
Christian Sayson Romano <romanozchristian@gmail.com>
2026-03-31 01:44:49 +0800

    Update data.js

--- data.js
+++ data.js
@@ -42,6 +42,41 @@ export const projectsData = [
         description: "Lorem ipsum mwa mwa",
         gradient: "from-rose-900/30 to-black",
     },
+  {
+        img: "/assets/images/byroun.png",
+        category: "web",
+        title: "Project",
+        description: "Lorem ipsum mwa mwa",
+        gradient: "from-gold/20 to-black",
+    },
+    {
+        img: "/assets/images/byroun.png",
+        category: "brand",
+        title: "Project",
+        description: "Lorem ipsum mwa mwa",
+        gradient: "from-purple-900/40 to-black",
+    },
+    {
+        img: "/assets/images/byroun.png",
+        category: "video",
+        title: "Project",
+        description: "Lorem ipsum mwa mwa",
+        gradient: "from-sky-900/40 to-black",
+    },
+    {
+        img: "/assets/images/byroun.png",
+        category: "web",
+        title: "Project",
+        description: "Lorem ipsum mwa mwa",
+        gradient: "from-emerald-900/30 to-black",
+    },
+    {
+        img: "/assets/images/byroun.png",
+        category: "brand",
+        title: "idk",
+        description: "Lorem ipsum mwa mwa",
+        gradient: "from-rose-900/30 to-black",
+    },
 ];
 
   // ── Team Data ─────────────────────────────────────
